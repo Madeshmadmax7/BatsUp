3a9eda81c8821f36eefac65a0b1ff104928e900f
MADMAX <amadesh702@gmail.com>
2026-04-16 21:51:59 +0530

    ui update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import viteDevErrorForward from './vite-plugins/vite-dev-error-forward'
 
 // https://vite.dev/config/
 export default defineConfig({
-  
-  plugins:[tailwindcss(),react()]
-})
+  plugins: [react(), tailwindcss(), viteDevErrorForward()],
+});
